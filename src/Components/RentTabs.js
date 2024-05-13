5212d1bc2cae95fff36477c92026c2c7aeec7de6
swapniljrni <spatil@jrni.com>
2024-05-13 13:23:47 +0530

    minor changes

--- src/Components/RentTabs.js
+++ src/Components/RentTabs.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function RentTabs({ onTabClick }) {
     const [activeTab, setActiveTab] = useState('properties');
-    const handleTabClick = tabName => {
+
+    useEffect(() => {
         if (onTabClick) {
-            onTabClick(tabName);
+            onTabClick(activeTab);
         }
-    };
+    }, [activeTab, onTabClick]);
 
     return (
         <div className='rent-tabs-container'>
             <nav>
                 <ul className='rent-tabs-flex'>
-                    <button className={activeTab === 'requirements' ? 'active' : ''} onClick={() => handleTabClick('requirements')}>Requirements</button>
-                    <button className={activeTab === 'properties' ? 'active' : ''} onClick={() => handleTabClick('properties')}>Properties</button>
-                    <button className={activeTab === 'contact' ? 'active' : ''} onClick={() => handleTabClick('contact')}>Contact</button>
-                    <button className={activeTab === 'offers' ? 'active' : ''} onClick={() => handleTabClick('offers')}>Offers</button>
-                    <button className={activeTab === 'documents' ? 'active' : ''} onClick={() => handleTabClick('documents')}>Documents</button>
+                    <button className={activeTab === 'requirements' ? 'active' : ''} onClick={() => setActiveTab('requirements')}>Requirements</button>
+                    <button className={activeTab === 'properties' ? 'active' : ''} onClick={() => setActiveTab('properties')}>Properties</button>
+                    <button className={activeTab === 'contact' ? 'active' : ''} onClick={() => setActiveTab('contact')}>Contact</button>
+                    <button className={activeTab === 'offers' ? 'active' : ''} onClick={() => setActiveTab('offers')}>Offers</button>
+                    <button className={activeTab === 'documents' ? 'active' : ''} onClick={() => setActiveTab('documents')}>Documents</button>
                 </ul>
-
             </nav>
         </div>
     );
